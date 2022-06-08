209eeb4e5bcebfde3d8d6ade75f5efbf9f439d9a
adaokudo <oku22001@byui.edu>
2022-06-08 20:15:44 +0100

    commit on "main'

--- scripts/Task1.js
+++ scripts/Task1.js
@@ -1,13 +1,11 @@
-const myInfo = {
-    photo:  'images/Adaobi okudo.jpg',
-}
+const photo= 'images/adaobi.jpg'
 
 
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
-document.querySelector('#photo').setAttribute('src', myInfo.photo);
+document.querySelector('#photo').setAttribute('src', photo);
 
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
-document.querySelector('#photo').setAttribute('alt', myInfo.photo);
+document.querySelector('#photo').setAttribute('alt', photo);
 
 
 
