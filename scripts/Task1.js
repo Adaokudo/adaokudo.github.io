80fa077820548a87447c2c6fbe7d5f070617fe3b
adaokudo <oku22001@byui.edu>
2022-06-09 19:56:52 +0100

    Updating Assignment

--- scripts/Task1.js
+++ scripts/Task1.js
@@ -1,11 +1,12 @@
 const photo= 'images/adaobi.jpg'
 
-
-// Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
 document.querySelector('#photo').setAttribute('src', photo);
 
-// Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
+
 document.querySelector('#photo').setAttribute('alt', photo);
 
+const currentDate = new Date();
+
+document.querySelector('#year').textContent = currentDate.getFullYear();
 
 
